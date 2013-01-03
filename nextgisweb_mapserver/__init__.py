3cb7c8e29699e6ea83c08caa421f4fa1b424714d
Aleksandr Dezhin <me@dezhin.net>
2013-01-03 11:15:25 +0400

    Параметры соединения БД из компонента, а не из запроса

--- nextgisweb_mapserver/__init__.py
+++ nextgisweb_mapserver/__init__.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from nextgisweb.component import Component
 
-from .models import MapserverStyle
 
 @Component.registry.register
 class MapserverStyleComponent(Component):
-	identity = 'mapserver_style'
+    identity = 'mapserver_style'
+
+    def initialize(self):
+        Component.initialize(self)
+
+        from . import models
+        models.include(self)
+
 
 def amd_packages():
-	return (
-		('mapserver_style', 'nextgisweb_mapserver:amd_packages/mapserver_style'),
-	)
+    return (
+        ('mapserver_style', 'nextgisweb_mapserver:amd_packages/mapserver_style'),
+    )
