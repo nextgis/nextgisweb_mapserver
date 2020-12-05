7b8123ed802c9d42c4a133ac2448f8a00aff3541
Aleksandr Dezhin <me@dezhin.net>
2020-12-05 22:18:33 +0300

    Build mapscript locally

--- docker.py
+++ docker.py
@@ -9,7 +9,35 @@ class Package(PackageBase):
 
 @AppImage.on_apt.handler
 def on_apt(event):
-    event.package('python3-mapscript' if event.image.context.python3 else 'python-mapscript')
+    image = event.image
+
+    event.package('cmake', 'swig', 'libfreetype6-dev', 'libgeos-dev')
+    if event.image.context.python3:
+        event.package('python3-pip', 'python3-setuptools')
+    else:
+        event.package('python-pip', 'python-setuptools')
+
+    python = '/usr/bin/python3' if image.context.python3 else '/usr/bin/python2'
+    msver = '7.6.1'
+
+    event.pop()
+    event.command(
+        'cd /tmp',
+        'curl -sSL https://download.osgeo.org/mapserver/mapserver-{0}.tar.gz > mapserver-{0}.tar.gz'.format(msver),
+        'tar -zxf mapserver-{0}.tar.gz && rm mapserver-{0}.tar.gz'.format(msver),
+        'mv mapserver-{0} mapserver && cd mapserver'.format(msver),
+        'mkdir build && cd build',
+        'cmake -DCMAKE_INSTALL_PREFIX=/usr/local ' +
+            # Workaround: with dynamic linking _mapserver.so extension gets linked to build directory
+            '-DCMAKE_POSITION_INDEPENDENT_CODE=ON -DLINK_STATIC_LIBMAPSERVER=ON ' + 
+            '-DWITH_PROTOBUFC=OFF -DWITH_FRIBIDI=OFF -DWITH_HARFBUZZ=OFF -DWITH_CAIRO=OFF ' + 
+            '-DWITH_FCGI=OFF -DWITH_POSTGIS=OFF -DWITH_WFS=OFF -DWITH_WCS=OFF -DWITH_LIBXML2=OFF ' +
+            '-DWITH_PYTHON=ON -DPYTHON_EXECUTABLE={} ../ '.format(python) +
+            ' > ../configure.out.txt',
+        'make && make install',
+        'cd / && rm -rf /tmp/mapserver',
+    )
+
 
 
 @AppImage.on_package_files.handler
