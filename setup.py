b80cae507f958fddb020274bd3a60ea23e76666b
Aleksandr Dezhin <me@dezhin.net>
2020-12-06 05:37:50 +0300

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4",
     install_requires=requires,
     entry_points=entry_points,
 )
