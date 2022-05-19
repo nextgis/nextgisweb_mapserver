91dac5f4bd92e1d1f7829e8b7cb5b519a476c045
Aleksandr Dezhin <me@dezhin.net>
2022-05-20 01:25:06 +0300

    Don't use legacy object widgets for resources

--- nextgisweb_mapserver/view.py
+++ nextgisweb_mapserver/view.py
@@ -1,13 +1,6 @@
-from lxml import etree
-
 from nextgisweb.resource import Widget
-from nextgisweb.object_widget import ObjectWidget
-
-from .mapfile import schema
-from .extmapfile import Map
 
 from .model import MapserverStyle
-
 from .util import _
 
 
@@ -28,51 +21,4 @@ class StyleWidget(Widget):
 
 
 def setup_pyramid(comp, config):
-
-    class MapserverStyleObjectWidget(ObjectWidget):
-        def is_applicable(self):
-            return self.operation in ('create', 'edit')
-
-        def validate(self):
-            result = super(MapserverStyleObjectWidget, self).validate()
-
-            self.error = []
-
-            def err(msg):
-                self.error.append(dict(message=msg))
-                return False
-
-            try:
-                layer = etree.fromstring(self.data['xml'])
-
-                relaxng = schema(Map)
-                relaxng.assertValid(layer)
-
-            except etree.XMLSyntaxError as e:
-                result = err(_("XML syntax error: %s") % str(e))
-
-            except etree.DocumentInvalid as e:
-                result = err(_("XML schema error: %s") % str(e))
-
-            return result
-
-        def populate_obj(self):
-            super(MapserverStyleObjectWidget, self).populate_obj()
-            self.obj.xml = self.data['xml']
-
-        def widget_module(self):
-            return 'mapserver_style/Widget'
-
-        def widget_params(self):
-            result = super(MapserverStyleObjectWidget, self).widget_params()
-
-            if self.obj:
-                result['value'] = dict(xml=self.obj.xml)
-            else:
-                result['value'] = dict(
-                    xml=MapserverStyle.default_style_xml(self.options['parent'])
-                )
-
-            return result
-
-    MapserverStyle.object_widget = MapserverStyleObjectWidget
+    pass
