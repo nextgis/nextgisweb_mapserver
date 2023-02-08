007a54c65d4ac25e817c7bdc9f43f051699141b4
Aleksandr Dezhin <me@dezhin.net>
2023-02-08 19:50:51 +0300

    Fix python_requires syntax

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.8.*, <4",
+    python_requires=">=3.8,<4",
     install_requires=requires,
     entry_points=entry_points,
 )
