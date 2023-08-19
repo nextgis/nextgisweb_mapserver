3e43ff54243c6bc86894d006b605d65ab3f8f4dc
Aleksandr Dezhin <me@dezhin.net>
2023-08-19 02:06:52 +0000

    Clean up and format tests

--- nextgisweb_mapserver/test/test_render.py
+++ nextgisweb_mapserver/test/test_render.py
@@ -1,43 +1,28 @@
 from pathlib import Path
-from uuid import uuid4
 
 import pytest
 import transaction
-from osgeo import ogr
 
 from nextgisweb.env import DBSession
 
-from nextgisweb.auth import User
 from nextgisweb.spatial_ref_sys import SRS
 from nextgisweb.vector_layer import VectorLayer
 
 from ..model import MapserverStyle
 
-data_path = Path(__file__).parent / 'data'
+pytestmark = pytest.mark.usefixtures("ngw_resource_defaults")
+
+data_path = Path(__file__).parent / "data"
 
 
 @pytest.fixture()
-def layer(ngw_resource_group):
+def layer():
     with transaction.manager:
-        res = VectorLayer(
-            parent_id=ngw_resource_group, display_name='test-vlayer',
-            owner_user=User.by_keyname('administrator'),
-            srs=SRS.filter_by(id=3857).one(),
-            tbl_uuid=uuid4().hex
-        ).persist()
-
-        ds = ogr.Open(str(data_path / 'multipolygonz-duplicate.geojson'))
-        layer = ds.GetLayer(0)
-
-        res.setup_from_ogr(layer)
-        res.load_from_ogr(layer)
-
+        source = data_path / "multipolygonz-duplicate.geojson"
+        res = VectorLayer().persist().from_ogr(source)
         DBSession.flush()
 
     yield res
-
-    with transaction.manager:
-        DBSession.delete(res)
 
 
 color = (200, 0, 0)
@@ -47,16 +32,13 @@ color = (200, 0, 0)
 def style(layer):
     with transaction.manager:
         res = MapserverStyle(
-            parent_id=layer.id, display_name='test-msstyle',
-            owner_user=User.by_keyname('administrator'),
+            parent_id=layer.id,
             xml=MapserverStyle.default_style_xml(layer, color=color),
         ).persist()
 
         DBSession.flush()
 
         yield res
-
-        DBSession.delete(res)
 
 
 def test_render(style):
@@ -78,11 +60,16 @@ def test_render(style):
         (size - p, size - p),
         (center, center),
     ):
-        assert img.getpixel((x, y)) == color + (255, )
+        assert img.getpixel((x, y)) == color + (255,)
 
     for dx, dy in (
-        (h, 0), (h, -h), (0, -h),
-        (-h, -h), (-h, 0), (-h, h),
-        (0, h), (h, h),
+        (h, 0),
+        (h, -h),
+        (0, -h),
+        (-h, -h),
+        (-h, 0),
+        (-h, h),
+        (0, h),
+        (h, h),
     ):
         assert img.getpixel((center + dx, center + dy)) == (0, 0, 0, 0)
