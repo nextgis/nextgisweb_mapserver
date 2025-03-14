25cf1ecec8d5a0f644beb978277dc0de11404eaa
Aleksandr Dezhin <me@dezhin.net>
2025-03-14 23:49:29 +0000

    Clean up type imports

--- nextgisweb_mapserver/nodepkg/editor-widget/EditorWidget.tsx
+++ nextgisweb_mapserver/nodepkg/editor-widget/EditorWidget.tsx
@@ -2,25 +2,23 @@ import { observer } from "mobx-react-lite";
 
 import { Code } from "@nextgisweb/gui/component/code";
 import { gettext } from "@nextgisweb/pyramid/i18n";
-import type {
-    EditorWidgetComponent,
-    EditorWidgetProps,
-} from "@nextgisweb/resource/type";
+import type { EditorWidget as IEditorWidget } from "@nextgisweb/resource/type";
 
 import type { EditorStore } from "./EditorStore";
 
-export const EditorWidget: EditorWidgetComponent<
-    EditorWidgetProps<EditorStore>
-> = observer(({ store }: EditorWidgetProps<EditorStore>) => {
-    return (
-        <Code
-            value={store.xml.in || undefined}
-            onChange={(v) => (store.xml.out = v)}
-            lang="xml"
-            lineNumbers
-        ></Code>
-    );
-});
+export const EditorWidget: IEditorWidget<EditorStore> = observer(
+    ({ store }) => {
+        return (
+            <Code
+                value={store.xml.in || undefined}
+                onChange={(v) => (store.xml.out = v)}
+                lang="xml"
+                lineNumbers
+            ></Code>
+        );
+    }
+);
 
+EditorWidget.displayName = "EditorWidget";
 EditorWidget.title = gettext("MapServer style");
 EditorWidget.activateOn = { create: true };
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with io.open("VERSION", "r") as fd:
     VERSION = fd.read().rstrip()
 
 requires = [
-    "nextgisweb>=5.0.0.dev13",
+    "nextgisweb>=5.1.0.dev3",
     "ply",
 ]
 
